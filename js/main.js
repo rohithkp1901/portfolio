19285aa637316ce4f950c6af59c6aae70a1ca8eb
rohithkp1901 <rohithkp1901@gmail.com>
2025-09-09 13:47:07 +0530

    made somemore changes34 email

--- js/main.js
+++ js/main.js
@@ -532,68 +532,130 @@ var jarallaxPlugin = function() {
 	});
 };
 
-var contactForm = function() {
-	if ($('#contactForm').length > 0 ) {
-		$( "#contactForm" ).validate( {
-			rules: {
-				name: "required",
-				email: {
-					required: true,
-					email: true
-				},
-				message: {
-					required: true,
-					minlength: 5
-				}
-			},
-			messages: {
-				name: "Please enter your name",
-				email: "Please enter a valid email address",
-				message: "Please enter a message"
-			},
-			errorElement: 'span',
-			errorLabelContainer: '.form-error',
-			/* submit via ajax */
-			submitHandler: function(form) {		
-				var $submit = $('.submitting'),
-					waitText = 'Submitting...';
+// var contactForm = function() {
+// 	if ($('#contactForm').length > 0 ) {
+// 		$( "#contactForm" ).validate( {
+// 			rules: {
+// 				name: "required",
+// 				email: {
+// 					required: true,
+// 					email: true
+// 				},
+// 				message: {
+// 					required: true,
+// 					minlength: 5
+// 				}
+// 			},
+// 			messages: {
+// 				name: "Please enter your name",
+// 				email: "Please enter a valid email address",
+// 				message: "Please enter a message"
+// 			},
+// 			errorElement: 'span',
+// 			errorLabelContainer: '.form-error',
+// 			/* submit via ajax */
+// 			submitHandler: function(form) {		
+// 				var $submit = $('.submitting'),
+// 					waitText = 'Submitting...';
 
-				$.ajax({   	
-			      type: "POST",
-			      url: "php/send-email.php",
-			      data: $(form).serialize(),
+// 				$.ajax({   	
+// 			      type: "POST",
+// 			      url: "php/send-email.php",
+// 			      data: $(form).serialize(),
 
-			      beforeSend: function() { 
-			      	$submit.css('display', 'block').text(waitText);
-			      },
-			      success: function(msg) {
-	               if (msg == 'OK') {
-	               	$('#form-message-warning').hide();
-			            setTimeout(function(){
-	               		$('#contactForm').fadeOut();
-	               	}, 1000);
-			            setTimeout(function(){
-			               $('#form-message-success').fadeIn();   
-	               	}, 1400);
+// 			      beforeSend: function() { 
+// 			      	$submit.css('display', 'block').text(waitText);
+// 			      },
+// 			      success: function(msg) {
+// 	               if (msg == 'OK') {
+// 	               	$('#form-message-warning').hide();
+// 			            setTimeout(function(){
+// 	               		$('#contactForm').fadeOut();
+// 	               	}, 1000);
+// 			            setTimeout(function(){
+// 			               $('#form-message-success').fadeIn();   
+// 	               	}, 1400);
 		               
-		            } else {
-		               $('#form-message-warning').html(msg);
-			            $('#form-message-warning').fadeIn();
-			            $submit.css('display', 'none');
-		            }
-			      },
-			      error: function() {
-			      	$('#form-message-warning').html("Something went wrong. Please try again.");
-			         $('#form-message-warning').fadeIn();
-			         $submit.css('display', 'none');
-			      }
-		      });    		
-	  		}
+// 		            } else {
+// 		               $('#form-message-warning').html(msg);
+// 			            $('#form-message-warning').fadeIn();
+// 			            $submit.css('display', 'none');
+// 		            }
+// 			      },
+// 			      error: function() {
+// 			      	$('#form-message-warning').html("Something went wrong. Please try again.");
+// 			         $('#form-message-warning').fadeIn();
+// 			         $submit.css('display', 'none');
+// 			      }
+// 		      });    		
+// 	  		}
 			
-		} );
-	}
-};
+// 		} );
+// 	}
+// };
+var contactForm = function() {
+  if ($('#contactForm').length > 0) {
+    $("#contactForm").validate({
+      rules: {
+        name: "required",
+        email: {
+          required: true,
+          email: true
+        },
+        message: {
+          required: true,
+          minlength: 5
+        }
+      },
+      messages: {
+        name: "Please enter your name",
+        email: "Please enter a valid email address",
+        message: "Please enter a message"
+      },
+      errorElement: 'span',
+      errorLabelContainer: '.form-error',
+      /* submit via ajax */
+      submitHandler: function(form) {
+        var $submit = $('.submitting'),
+          waitText = 'Submitting...';
 
+        $.ajax({
+          type: "POST",
+          url: "https://formspree.io/f/mdklboly",
+          data: $(form).serialize(),
+          headers: {
+            'Accept': 'application/json' // Add this header
+          },
+          beforeSend: function() {
+            $submit.css('display', 'block').text(waitText);
+          },
+          success: function(msg) {
+            // Formspree returns a JSON object, not 'OK'
+            // We can check for the presence of the message
+            if (msg.ok === true) {
+              $('#form-message-warning').hide();
+              setTimeout(function() {
+                $('#contactForm').fadeOut();
+              }, 1000);
+              setTimeout(function() {
+                $('#form-message-success').fadeIn();
+              }, 1400);
+            } else {
+              $('#form-message-warning').html("Something went wrong. Please try again.");
+              $('#form-message-warning').fadeIn();
+              $submit.css('display', 'none');
+            }
+          },
+          error: function() {
+            $('#form-message-warning').html("Something went wrong. Please try again.");
+            $('#form-message-warning').fadeIn();
+            $submit.css('display', 'none');
+          }
+        });
+      }
+    });
+  }
+};
 var stickyFillPlugin = function() {
 	var elements = document.querySelectorAll('.unslate_co--sticky');
 	Stickyfill.add(elements);
